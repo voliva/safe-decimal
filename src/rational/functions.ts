714c8bf521e62fded5664027c458a446f6e1d1f7
Víctor Oliva <olivarra1@gmail.com>
2022-04-23 13:09:30 +0200

    refactor Rational to {n, d}

--- src/rational/functions.ts
+++ src/rational/functions.ts
@@ -1,7 +1,10 @@
 import { DivisionByZeroError, NegativeRootError } from "../common";
 
 // Only the first number can have sign
-export type RationalNumber = [bigint, bigint];
+export interface RationalNumber {
+  readonly n: bigint;
+  readonly d: bigint;
+}
 
 // Assumes only one `.` as decimal separator + optional starting symbol, no exponent.
 export function fromDecimalString(s: string): RationalNumber {
@@ -10,7 +13,7 @@ export function fromDecimalString(s: string): RationalNumber {
   const d = BigInt(10) ** BigInt(decimals);
   const n = BigInt(s.replace(".", ""));
 
-  return reduce([n, d]);
+  return reduce({ n, d });
 }
 
 export function fromNumber(n: number) {
@@ -20,7 +23,7 @@ export function fromNumber(n: number) {
 }
 
 export function toNumber(a: RationalNumber) {
-  return Number(a[0]) / Number(a[1]);
+  return Number(a.n) / Number(a.d);
 }
 
 export function toDecimalString(
@@ -28,16 +31,16 @@ export function toDecimalString(
   options?: { radix: number; maxDecimals: number }
 ) {
   const { radix = 10, maxDecimals = 10 } = options ?? {};
-  const sign = a[0] < 0n ? "-" : "";
-  const num = s_abs(a[0]);
-  const den = a[1];
+  const sign = a.n < 0n ? "-" : "";
+  const num = s_abs(a.n);
+  const den = a.d;
 
   // Split fractional part from integer part
   const integerPart = num / den;
   const fractionalPart = num - integerPart * den;
 
   if (fractionalPart === 0n) {
-    return sign + integerPart.toString(Number(options.radix));
+    return sign + integerPart.toString(radix);
   }
 
   let decimalStr = "";
@@ -58,63 +61,63 @@ export function toDecimalString(
   -> Push `2` to decimalStr
   -> update a/b = a/b - 2/27
   */
-  let fraction: RationalNumber = [fractionalPart, den];
+  let fraction: RationalNumber = { n: fractionalPart, d: den };
   const bigRadix = BigInt(radix);
   let power = bigRadix;
-  while (fraction[0] !== 0n && decimalStr.length < maxDecimals) {
+  while (fraction.n !== 0n && decimalStr.length < maxDecimals) {
     /** To compare x / p < a / b, we can simplify:
      * (x / p) < (a / b)
      * => ((x * b) / (p * b)) < ((a * p) / (p * b))
      * => (x * b) < (a * p)
      * => x < (a * p) / b
      */
-    const x = (fraction[0] * power) / fraction[1];
+    const x = (fraction.n * power) / fraction.d;
     decimalStr += x;
-    fraction = sub(fraction, [x, power]);
+    fraction = sub(fraction, { n: x, d: power });
     power = power * bigRadix;
   }
 
-  return sign + integerPart.toString(Number(options.radix)) + "." + decimalStr;
+  return sign + integerPart.toString(radix) + "." + decimalStr;
 }
 
 export function neg(a: RationalNumber): RationalNumber {
-  return [-a[0], a[1]];
+  return { n: -a.n, d: a.d };
 }
 export function abs(a: RationalNumber): RationalNumber {
-  return a[0] >= 0 ? a : neg(a);
+  return a.n >= 0 ? a : neg(a);
 }
 export function inv(a: RationalNumber): RationalNumber {
-  if (a[0] === 0n) {
+  if (a.n === 0n) {
     throw new DivisionByZeroError();
   }
 
-  return a[0] > 0 ? [a[1], a[0]] : [-a[1], -a[0]];
+  return a.n > 0 ? { n: a.d, d: a.n } : { n: -a.d, d: -a.n };
 }
 export function add(a: RationalNumber, b: RationalNumber) {
-  const lcm_val = lcm(a[1], b[1]);
-  const num = (a[0] * lcm_val) / a[1] + (b[0] * lcm_val) / b[1];
+  const lcm_val = lcm(a.d, b.d);
+  const num = (a.n * lcm_val) / a.d + (b.n * lcm_val) / b.d;
 
   // Reduce still needed: e.g. 2/3 + 5/6 = (4+5)/6 = 9/6 = 3/2
-  return reduce([num, lcm_val]);
-  // return reduce([a[0] * b[1] + b[0] * a[1], a[1] * b[1]]); TODO Benchmark which one is faster
+  return reduce({ n: num, d: lcm_val });
+  // return reduce([a.n * b.d + b.n * a.d, a.d * b.d]); TODO Benchmark which one is faster
 }
 export function sub(a: RationalNumber, b: RationalNumber) {
   return add(a, neg(b));
 }
 export function mul(a: RationalNumber, b: RationalNumber) {
-  return reduce([a[0] * b[0], a[1] * b[1]]);
+  return reduce({ n: a.n * b.n, d: a.d * b.d });
 }
 export function div(a: RationalNumber, b: RationalNumber) {
   return mul(a, inv(b));
 }
 
 export function exp(a: RationalNumber, b: bigint): RationalNumber {
-  return [a[0] ** b, a[1] ** b];
+  return { n: a.n ** b, d: a.d ** b };
 }
 
 // Assumes number is reduced. -1 = a is bigger, 0 = both are equal, 1 = b is bigger.
 export function cmp(a: RationalNumber, b: RationalNumber): -1 | 0 | 1 {
-  return s_cmp(a[0] * b[1], a[1] * b[0]);
+  return s_cmp(a.n * b.d, a.d * b.n);
 }
 function s_cmp(a: bigint, b: bigint): -1 | 0 | 1 {
   if (a === b) return 0;
@@ -127,26 +130,27 @@ function s_cmp(a: bigint, b: bigint): -1 | 0 | 1 {
 // This function takes a RationalNumber and returns another with smaller denominator that approximates it.
 // Mhh.... sometimes it doesn't really aproximate, e.g. approx([173187621363126874672123123423421n,173187621363126874672123123423423n], 4), or any 1/n really
 export function approx(a: RationalNumber, depth: number): RationalNumber {
-  const sign = a[0] < 0 ? -1n : 1n;
+  const sign = a.n < 0 ? -1n : 1n;
 
-  const integerPart = s_abs(a[0]) / a[1];
-  const fractionalPart = s_abs(a[0]) - integerPart * a[1];
+  const integerPart = s_abs(a.n) / a.d;
+  const fractionalPart = s_abs(a.n) - integerPart * a.d;
 
-  if (depth <= 0 || fractionalPart === 0n) return [sign * integerPart, 1n];
+  if (depth <= 0 || fractionalPart === 0n)
+    return { n: sign * integerPart, d: 1n };
 
   const result = add_integer(
-    inv(approx([a[1], fractionalPart], depth - 1)),
+    inv(approx({ n: a.d, d: fractionalPart }, depth - 1)),
     integerPart
   );
-  return [sign * result[0], result[1]];
+  return { n: sign * result.n, d: result.d };
 }
 function add_integer(a: RationalNumber, int: bigint): RationalNumber {
-  return [a[0] + int * a[1], a[1]];
+  return { n: a.n + int * a.d, d: a.d };
 }
 
 // Very rough approximation
 export function root(a: RationalNumber, b: bigint): RationalNumber {
-  return [s_root(a[0], b), s_root(a[1], b)];
+  return { n: s_root(a.n, b), d: s_root(a.d, b) };
 }
 // https://golb.hplar.ch/2018/09/javascript-bigint.html
 // https://stackoverflow.com/questions/1375953/how-to-calculate-an-arbitrary-power-root
@@ -182,12 +186,12 @@ function s_root(a: bigint, b: bigint) {
   //   x = (bx - x + a / x ** (b - 1n)) / b;
   // }
 
-  // return x;
+  return x;
 }
 
 export function pow(a: RationalNumber, b: RationalNumber) {
-  const result = root(exp(a, s_abs(b[0])), b[1]);
-  if (a[0] < 0n) {
+  const result = root(exp(a, s_abs(b.n)), b.d);
+  if (a.n < 0n) {
     return inv(result);
   }
   return result;
@@ -213,11 +217,11 @@ function lcm(a: bigint, b: bigint) {
   return (a * b) / gcd(a, b);
 }
 // Assumes a valid number (d !== 0n)
-function reduce([n, d]: RationalNumber): RationalNumber {
+function reduce({ n, d }: RationalNumber): RationalNumber {
   if (n === 0n) {
-    return [0n, 1n];
+    return { n: 0n, d: 1n };
   }
 
   const divisor = gcd(n, d);
-  return [n / divisor, d / divisor];
+  return { n: n / divisor, d: d / divisor };
 }
